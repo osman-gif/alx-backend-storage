19e26914df2070e6fc4d1ab3c1a85ac367afd742
osman-gif <osmanngong@gmail.com>
2024-10-19 10:09:45 +0200

    Modified the 0-uniq_users.sql file

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -10,7 +10,7 @@
 -- Context: Make an attribute unique directly in the table schema will enforced your business rules and avoid bugs in your application
 
 CREATE TABLE IF NOT EXISTS users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255)
 );
