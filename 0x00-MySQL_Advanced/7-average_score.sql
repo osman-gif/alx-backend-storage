a7954780af9a4898afeebf96acaecad343a63fff
osman-gif <osmanngong@gmail.com>
2024-10-19 11:50:25 +0200

    Modified the 7-average_score to use corrections.user_id instead of user in where clause

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -11,6 +11,6 @@ DELIMITER //
 CREATE PROCEDURE ComputeAverageScoreForUser(user_id INT)
 BEGIN
     DECLARE avg_score DECIMAL(10, 2);
-    SELECT AVG(score) INTO avg_score FROM corrections WHERE user_id = user_id;
+    SELECT AVG(score) INTO avg_score FROM corrections WHERE user_id = corrections.user_id;
     UPDATE users SET average_score = avg_score WHERE id = user_id;
 END;
